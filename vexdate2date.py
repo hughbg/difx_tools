02cba5b7115c599d968afc0f2e22d4df4c6d595b
Hugh Garsden <hughgarsden@yahoo.com>
2025-06-27 17:11:09 +0100

    New date script

--- vexdate2date.py
+++ vexdate2date.py
@@ -17,8 +17,12 @@ if len(sys.argv) == 3:
 elif len(sys.argv) == 2:
     shift = 0
 else:
-    print("Usage: vexdat2vdifdate.py vex_date [seconds]")
-    print("Example: vexdate2vdifdate.py 2012y289d00h00m00s 5851")
+    print("Usage: vexdat2vdifdate.py vex_date [seconds offset]")
+    print("Example: vexdate2vdifdate.py 2000y001d00h00m00s 585")
+    print()
+    print("Usually you would get the arguments from a lines in a vex file in a 'scan'. Like these: ")
+    print("\tstart=2000y001d00h00m00s; mode=lba3cm-2p-2IF; source=0208-512;")
+    print("\tstation=Pa:    0 sec:  585 sec:  150.958 GB:   0 :       : 1;")
     exit(1)
 
 d = shift_time(sys.argv[1], shift)
--- vexdate2vdifdate.py
+++ vexdate2vdifdate.py
@@ -20,17 +20,24 @@ if len(sys.argv) == 3:
 elif len(sys.argv) == 2:
     shift = 0
 else:
-    print("Usage: vexdat2vdifdate.py vex_date [seconds]")
-    print("Example: vexdate2vdifdate.py 2012y289d00h00m00s 5851")
+    print("Usage: vexdat2vdifdate.py vex_date [seconds offset]")
+    print("Example: vexdate2vdifdate.py 2000y001d00h00m00s 585")
+    print()
+    print("Usually you would get the arguments from a lines in a vex file in a 'scan'. Like these: ")
+    print("\tstart=2000y001d00h00m00s; mode=lba3cm-2p-2IF; source=0208-512;")
+    print("\tstation=Pa:    0 sec:  585 sec:  150.958 GB:   0 :       : 1;")
+    print()
+    print("Outputs ref_epoc and seconds_from_ref_epoch as integers, for use in fake VDIF")
+
     exit(1)
 
 d = shift_time(sys.argv[1], shift)
 input_date_as_str = d.strftime("%Y-%m-%d %H:%M:%S")
 print("Input date including seconds (if any):", input_date_as_str)
 
-ref_epoch = datetime.datetime(d.year, 1 if d.month<6 else 6, 1)
+ref_epoch = datetime.datetime(d.year, 1 if d.month<7 else 7, 1)
 six_month_intervals = (ref_epoch.year-2000)*2+ref_epoch.month//6
-print("ref_epoch (6 month intervals):", six_month_intervals, "=", ref_epoch.strftime("%Y-%m-%d %H:%M:%S"))
+print("ref_epoch (6 month intervals):", six_month_intervals, "(->", ref_epoch.strftime("%Y-%m-%d %H:%M:%S")+")")
 
 diff = d-ref_epoch
 seconds_from_ref_epoch = diff.days*24*60*60+diff.seconds
